971d137744b17bce45dde1f599ca378af0d5f065
МАРИЯ <khionakimary@yandex.ru>
2022-12-26 10:28:29 +0300

    Задача6

---
+++ Задача6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая на вход принимает число и выдает, является ли число четным (делится на 2 без остатка).
+
+Console.WriteLine("введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number%2 == 0)
+
+{
+    Console.WriteLine("да");
+
+}
+else
+{
+    Console.WriteLine("нет");
+}
