5a8d1e7f0790f7eb8b46e7643b103b7eb82cbaee
МАРИЯ <khionakimary@yandex.ru>
2022-12-26 09:01:21 +0300

    Задача2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая на вход принимает два числа и выдает какое больше, а какое меньшее
+
+Console.WriteLine("введите первое число: ");
+int firstnumber = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите второе число: ");
+int secondnumber = int.Parse(Console.ReadLine()!);
+
+int max = firstnumber;
+
+if (firstnumber < secondnumber) max = secondnumber;
+{
+    Console.Write("max = ");
+    Console.WriteLine(max);
+}
