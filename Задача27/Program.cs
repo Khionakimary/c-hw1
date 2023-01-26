9fdb5244b9c13d1eaec8d028137208b1e74a719a
МАРИЯ <khionakimary@yandex.ru>
2023-01-26 19:49:51 +0300

    Задача27

---
+++ Задача27/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int getUserDate(string message) 
+{ 
+    Console.WriteLine(message); 
+    int userData = int.Parse(Console.ReadLine()!); 
+    return userData; 
+} 
+int getDigitSum(int number) 
+{ 
+    int sum = 0; 
+    while (number > 0) 
+    { 
+        sum = sum + number % 10; 
+        number = number / 10; 
+    } 
+    return sum; 
+} 
+void showResult (string messageStart, int data) 
+{ 
+    Console.Write(messageStart); 
+    Console.Write(data); 
+     
+}  
+int number = getUserDate("Введите число"); 
+int sum = getDigitSum(number); 
+showResult($"Сумма цифр в числе {number} = ", sum);
+
