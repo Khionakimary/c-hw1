67cb2f3a5d4239b0f6b4b6c9dc98e54c55d10607
МАРИЯ <khionakimary@yandex.ru>
2023-02-02 09:26:56 +0300

    Задача38

---
+++ Задача38/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+//Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+int[] getGenerateArray(int length, int start, int end)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(start, end + 1);
+    }
+    return result;
+}
+void printArray(int[] array)
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i  < array.Length -1)
+        {
+           Console.Write($", ");
+        }
+    }
+    Console.WriteLine($"]");
+}
+int getMin(int[] array)
+{
+    int min = array[0];
+    for (int i = 0; i < array.Length; i++)
+        {
+          if (array[i] < min)
+                {
+                    min = array[i];
+                } 
+            }
+            return min;
+}
+int getMax(int[] array)
+{
+    int max = array[0];
+    for (int i = 0; i < array.Length; i++)
+        {
+          if (array[i] > max)
+                {
+                    max = array[i];
+                } 
+            }
+            return max;
+}
+int getDiferenceBetweenMaxAndMin(int max, int min)
+{
+    int dif = max - min;
+    return dif;
+}
+int[] Array = getGenerateArray(10,0,50);
+printArray(Array);
+int min = getMin(Array);
+Console.WriteLine($"Минимальное число массива {min}");
+int max = getMax(Array);
+Console.WriteLine($"Максимальное число массива {max}");
+int diference = getDiferenceBetweenMaxAndMin(max,min);
+Console.WriteLine($"Разница между максимальным и минимальным числами в массиве равна {diference}");
